eaa8af79430e242e5a7d5e656471f0b91ff244c4
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-20 12:57:50 +0700

    Background start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PlayerArray from './components/PlayerArray/PlayerArray';
+import Background from './components/Background/Background';
 import './App.css'
 
 function App() {
   return (
     <>
+      <Background />
       <PlayerArray />
     </>
   )
