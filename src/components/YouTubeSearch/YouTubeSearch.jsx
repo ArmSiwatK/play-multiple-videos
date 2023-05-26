690b94e0014215883e89f95a057ccac1731ed642
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-26 14:57:19 +0700

    fix: Update fetch url

--- src/components/YouTubeSearch/YouTubeSearch.jsx
+++ src/components/YouTubeSearch/YouTubeSearch.jsx
@@ -13,7 +13,7 @@ const YouTubeSearch = () => {
 
     const handleSearch = async () => {
         try {
-            const response = await fetch('http://localhost:5000/api/search', {
+            const response = await fetch('https://backend-play-multiple-videos.onrender.com/api/search', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
