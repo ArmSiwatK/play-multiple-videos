c505fe2e75d36d286b9f24c65c62aa45297b6e21
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-26 14:11:34 +0700

    feat: Click to copy link

--- src/components/YouTubeSearch/YouTubeSearch.jsx
+++ src/components/YouTubeSearch/YouTubeSearch.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import './YouTubeSearch.css';
 
 const YouTubeSearch = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+
+
 
     const handleSearch = async () => {
         try {
@@ -25,21 +28,44 @@ const YouTubeSearch = () => {
         }
     };
 
+    const handleCardClick = async (videoId) => {
+        const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+        try {
+            await navigator.clipboard.writeText(videoUrl);
+            console.log('Video URL copied to clipboard:', videoUrl);
+        } catch (error) {
+            console.error('Failed to copy video URL:', error);
+        }
+    };
+
+
+
     return (
-        <div>
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                placeholder="Enter search term"
-            />
-            <button onClick={handleSearch}>Search</button>
+        <div className="container">
+            <div className="input-container">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Enter search term"
+                />
+                <button onClick={handleSearch}>Search</button>
+            </div>
 
             <ul>
                 {searchResults.map((video) => (
                     <li key={video.id.videoId}>
-                        <h3>{video.snippet.title}</h3>
-                        <p>{video.snippet.description}</p>
+                        <div
+                            className="card"
+                            onClick={() => handleCardClick(video.id.videoId)}
+                        >
+                            <img
+                                src={video.snippet.thumbnails.default.url}
+                                alt="Video Thumbnail"
+                            />
+                            <h3>{video.snippet.title}</h3>
+                        </div>
                     </li>
                 ))}
             </ul>
