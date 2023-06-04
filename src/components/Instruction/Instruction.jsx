914eea41eaf3456cc36dcbef15c37a42fb4f344f
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-04 11:10:07 +0700

    update: Instruction

--- src/components/Instruction/Instruction.jsx
+++ src/components/Instruction/Instruction.jsx
@@ -8,13 +8,9 @@ const Instruction = () => {
             <ul>
                 <li>Search for YouTube videos. Click on one to assign them to an empty YouTube player, or manually copy and paste the links yourself.</li>
                 <li>Use the notepad provided to note down URLs or any other information.</li>
-                <li>
-                    The YouTube player accepts any YouTube link that contains a video ID,
-                    except for Shorts.
-                </li>
-                <li>
-                    Add or remove players using the control buttons.
-                </li>
+                <li>The YouTube player accepts any YouTube link that contains a video ID.</li>
+                <li>Add or remove players using the control buttons.</li>
+                <li>The first time around will always be slow. Patience.</li>
                 <li>If searching doesn't work, it likely means my YouTube Data API has reached its daily limit. Go to YouTube yourself for the time being and try again tomorrow.</li>
                 <li>Here, a button for your convenience.</li>
             </ul>
