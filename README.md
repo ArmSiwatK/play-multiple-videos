db6e974ffb923ca9cc8f1238420616cc88a72690
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-04 11:29:37 +0700

    update: Instruction and README

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 # Play Multiple YouTube Videos
+
 A React application that allows users to view YouTube videos, play audio/video files, and take notes. Why would anybody want to play multiple YouTube videos from the convenience of a single page? Well, I don't know about other people, but I occasionally want to include ambient noises and/or other noises for a personalized, immersive listening experience. Manually playing multiple audio/videos at a time is a hassle.
 
 ## Installation
+
 1. Clone the repository:
    ```
    git clone https://github.com/ArmSiwatK/play-multiple-videos.git
    ```
 2. Navigate to the project directory:
-    ```
-    cd react-media-player-app
-    ```
+
+   ```
+   cd play-multiple-videos
+   ```
 
 3. Install dependencies:
-    ```
-    yarn install
-    ```
+
+   ```
+   yarn install
+   ```
 
 4. Start the development server:
-    ```
-    yarn run dev
-    ```
-
-## Usage
-* Enter a YouTube video link view a YouTube video.
-* Or upload audio or video files and play them.
-* Feel free to take notes like URLs.
+   ```
+   yarn run dev
+   ```
--- src/components/Instruction/Instruction.jsx
+++ src/components/Instruction/Instruction.jsx
@@ -7,11 +7,10 @@ const Instruction = () => {
             <h1>How to use:</h1>
             <ul>
                 <li>Search for YouTube videos. Click on one to assign them to an empty YouTube player, or manually copy and paste the links yourself.</li>
-                <li>Use the notepad provided to note down URLs or any other information.</li>
                 <li>The YouTube player accepts any YouTube link that contains a video ID.</li>
                 <li>Add or remove players using the control buttons.</li>
-                <li>The first time around will always be slow. Patience.</li>
-                <li>If searching doesn't work, it likely means my YouTube Data API has reached its daily limit. Go to YouTube yourself for the time being and try again tomorrow.</li>
+                <li>Searching the first time around will always be slow. Patience.</li>
+                <li>If searching really doesn't work, it most likely means my YouTube Data API has reached its daily limit. Go to YouTube yourself for the time being and try again tomorrow.</li>
                 <li>Here, a button for your convenience.</li>
             </ul>
         </div>
